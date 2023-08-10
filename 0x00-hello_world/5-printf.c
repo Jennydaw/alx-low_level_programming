bb06eb73f92803fbbe55e8b59c0c3e3da4f0e444
Jennydaw <jennesidaw@gmail.com>
2023-08-10 05:09:02 -0700

    Hello World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,9 @@
 *
 * Return: Always 0 (Success)
 */
-int main(world)
+int main(void)
 {
         printf("with proper grammar, but the outcome is a piece of art,\n");
-        return (0);	
+        return (0);
+}	
+		
