b8310f740816ef3fa5048edeb50abaf10b8dc6fe
Jennydaw <jennesidaw@gmail.com>
2023-08-10 17:11:09 -0700

    Hello World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
-#include<studio.h>
+#include <studio.h>
+
 /**
-* main - Entry point
+* main - A program with printf function
 *
-* Return: Always 0 (Success)
+* Return: 0 (Success)
 */
 int main(void)
-{
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-        return (0);
+{	
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }	
 		
