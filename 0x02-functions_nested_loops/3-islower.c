d6e1700017378e13ad902c74837d179558ee6363
Jennydaw <jennesidaw@gmail.com>
2023-08-15 10:57:10 -0700

    Putchar

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - Check for a character in lowercase
+ * @c: The character to be checked.
+ *
+ * Return: 1 for lowercase character or 0 for anything elase
+ */
+
+int _islower(int c)
+
+{
+
+	if (c >= '97' && c <= '122')
+	{	
+	return (1);
+	}
+	return (0);
+}
