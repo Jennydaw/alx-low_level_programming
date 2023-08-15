38ea3c1a9cbc5c111ff591a20b906f31997dba66
Jennydaw <jennesidaw@gmail.com>
2023-08-15 14:47:46 -0700

    Putchar

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - Alphabetic character checking
+ * @c: The character to be checked
+ *
+ * Return: 1 if character is letter, lowercase or uppercase, 0 otherwise.
+ */
+int _isalpha(int c)
+{
+if ((c >= 65 && c <= 97) ||(c >= 97 && c <= 122))
+{	
+return (1);
+}
+return (0);
+}
