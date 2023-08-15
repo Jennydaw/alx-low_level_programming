1b07a78160f6def039fc1ac29e8000f81c80f610
Jennydaw <jennesidaw@gmail.com>
2023-08-15 09:55:49 -0700

    Putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,10 @@
-#ifndef MAIN_HEADER FILE
-#define MAIN_HEADER_FILE
+#ifndef main_h
+#define main_h
 
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int _is lower(int c);
+int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
@@ -13,5 +13,6 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+void print_times_tablet(int n);
 
 #endif
