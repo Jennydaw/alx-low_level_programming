be438c0d4d0d620aa934574f3962e5e69e1b6a42
Jennydaw <jennesidaw@gmail.com>
2023-09-01 10:13:45 -0700

    Static libraries

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memset - Program fills a block of memory with a specific value
+ * @s: fill memory starting address 
+ * @b: the desired value
+ * @n: change number of bytes
+ *
+ * Return: change array with new value for n bytes
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+   	int a = 0;
+
+   	for (; n > 0; a++)
+   	{
+          	s[a] = b;
+          	n--;
+	}	
+	return (s);
+}
---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memcpy - function which copies memory area
+ * @dest: destination of memory area
+ * @src: memory area source
+ * @n: bytes filled
+ * Return: the pointer to dest.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*(dest + i) =  *(src + i);
+
+	return (dest);
+}
